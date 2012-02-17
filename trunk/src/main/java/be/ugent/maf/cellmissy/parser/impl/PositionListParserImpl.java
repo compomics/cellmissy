4f295f9f08fee590a3c2e51d328bccc6adce0238
paola.masuzzo <paola.masuzzo@bec5b0e4-4308-293c-41b1-17373d93ab8f>
2012-02-17 15:20:16 +0000

    add service annotation to PositionListParser

--- trunk/src/main/java/be/ugent/maf/cellmissy/parser/impl/PositionListParserImpl.java
+++ trunk/src/main/java/be/ugent/maf/cellmissy/parser/impl/PositionListParserImpl.java
@@ -16,11 +16,13 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import org.springframework.stereotype.Service;
 
 /**
  *
  * @author Paola
  */
+@Service("positionListParser")
 public class PositionListParserImpl implements PositionListParser {
 
     @Override
