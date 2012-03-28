3c0d4ee0ece678bc2335c9128115a276dae8f24e
nielshulstaert <nielshulstaert@bec5b0e4-4308-293c-41b1-17373d93ab8f>
2012-03-28 09:05:04 +0000

    added hibernate encryption added role enum

---
+++ src/main/java/be/ugent/maf/cellmissy/entity/Role.java
@@ -0,0 +1,14 @@
+package be.ugent.maf.cellmissy.entity;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: niels
+ * Date: 28/03/12
+ * Time: 10:57
+ * To change this template use File | Settings | File Templates.
+ */
+public enum Role {
+
+    ADMIN_USER, STANDARD_USER
+
+}
