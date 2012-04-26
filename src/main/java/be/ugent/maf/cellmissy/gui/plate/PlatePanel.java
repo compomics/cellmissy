f9d0784237a8c475c84c0972e479d3593e89bb20
paola.masuzzo@gmail.com <paola.masuzzo@gmail.com@bec5b0e4-4308-293c-41b1-17373d93ab8f>
2012-04-26 14:52:29 +0000

    modified PlateSetupPanelController with rectangle selection

--- src/main/java/be/ugent/maf/cellmissy/gui/plate/PlatePanel.java
+++ src/main/java/be/ugent/maf/cellmissy/gui/plate/PlatePanel.java
@@ -11,6 +11,7 @@ import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Point;
 import java.awt.Rectangle;
 import java.awt.RenderingHints;
 import java.awt.geom.Ellipse2D;
@@ -27,10 +28,44 @@ public class PlatePanel extends JPanel {
     private List<WellGui> wellGuiList;
     private PlateFormat plateFormat;
     private static final int pixelsGrid = 7;
-    private static final int pixelsBorders = 30;
-    private Rectangle currentRect;
-    private Rectangle rectToDraw;
+    private static final int pixelsBorders = 25;
+    private Point startPoint;
+    private Point endPoint;
+    private List<Rectangle> rectanglesToDrawList;
 
+    public List<WellGui> getWellGuiList() {
+        return wellGuiList;
+    }
+
+    public List<Rectangle> getRectanglesToDrawList() {
+        return rectanglesToDrawList;
+    }
+
+    public Point getEndPoint() {
+        return endPoint;
+    }
+
+    public Point getStartPoint() {
+        return startPoint;
+    }
+
+    public void setEndPoint(Point endPoint) {
+        this.endPoint = endPoint;
+    }
+
+    public void setStartPoint(Point startPoint) {
+        this.startPoint = startPoint;
+    }
+
+    public void initPanel(PlateFormat plateFormat, Dimension parentDimension) {
+        this.plateFormat = plateFormat;
+        wellGuiList = new ArrayList<>();
+        doResize(parentDimension);
+        rectanglesToDrawList = new ArrayList<>();
+        startPoint = null;
+        endPoint = null;
+    }
+    
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
@@ -43,34 +78,14 @@ public class PlatePanel extends JPanel {
         } else {
             reDrawWells(wellSize, g);
         }
-        
-        if(currentRect != null){
-            g.drawRect(rectToDraw.x, rectToDraw.y, rectToDraw.width - 1, rectToDraw.height - 1);
+
+        if (startPoint != null && endPoint != null) {
+            drawRect(g);
         }
-    }
 
-    public List<WellGui> getWellGuiList() {
-        return wellGuiList;
-    }
-
-    public Rectangle getCurrentRect() {
-        return currentRect;
-    }
-
-    public void setCurrentRect(Rectangle currentRect) {
-        this.currentRect = currentRect;
-    }
-
-    public Rectangle getRectToDraw() {
-        return rectToDraw;
-    }
-
-    public void initPanel(PlateFormat plateFormat, Dimension parentDimension) {
-        this.plateFormat = plateFormat;
-        wellGuiList = new ArrayList<>();
-        doResize(parentDimension);
-        currentRect = null;
-        rectToDraw = new Rectangle();
+        if (!rectanglesToDrawList.isEmpty()) {
+            drawRectangles(g);
+        }
     }
 
     public void drawWells(int wellSize, Graphics g) {
@@ -195,6 +210,35 @@ public class PlatePanel extends JPanel {
                 }
             }
             this.setSize(panelWidth, panelHeight);
+        }
+    }
+
+    private void drawRect(Graphics g) {
+
+        Graphics2D g2d = (Graphics2D) g;
+        setGraphics(g2d);
+        int x = Math.min(startPoint.x, endPoint.x);
+        int y = Math.min(startPoint.y, endPoint.y);
+        int width = Math.abs(startPoint.x - endPoint.x);
+        int height = Math.abs(startPoint.y - endPoint.y);
+        if (x + width > this.getWidth()) {
+            width = this.getWidth() - x;
+        }
+
+        if (y + height > this.getHeight()) {
+            height = this.getHeight() - y;
+        }
+
+        g2d.drawRect(x, y, width, height);
+
+    }
+
+    private void drawRectangles(Graphics g) {
+
+        Graphics2D g2d = (Graphics2D) g;
+        setGraphics(g2d);
+        for (Rectangle r : rectanglesToDrawList) {
+            g2d.drawRect(r.x, r.y, r.width, r.height);
         }
     }
 }
