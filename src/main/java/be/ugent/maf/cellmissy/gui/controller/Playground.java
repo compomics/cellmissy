5b067fb08cc71e6bf77f14fb3e56429d20c035eb
nielshulstaert@gmail.com <nielshulstaert@gmail.com@bec5b0e4-4308-293c-41b1-17373d93ab8f>
2013-06-17 11:44:14 +0000

    changed playground

--- src/main/java/be/ugent/maf/cellmissy/gui/controller/Playground.java
+++ src/main/java/be/ugent/maf/cellmissy/gui/controller/Playground.java
@@ -4,6 +4,7 @@
  */
 package be.ugent.maf.cellmissy.gui.controller;
 
+import be.ugent.maf.cellmissy.analysis.MultipleComparisonsCorrector;
 import be.ugent.maf.cellmissy.spring.ApplicationContextProvider;
 import org.hibernate.ejb.Ejb3Configuration;
 import org.hibernate.tool.hbm2ddl.SchemaExport;
@@ -21,16 +22,20 @@ public class Playground {
         // get the application context
         ApplicationContext context = ApplicationContextProvider.getInstance().getApplicationContext();
 
-        LocalContainerEntityManagerFactoryBean fb = (LocalContainerEntityManagerFactoryBean) context.getBean("&entityManagerFactory");
-
-        Ejb3Configuration cfg = new Ejb3Configuration();
-        Ejb3Configuration configured = cfg.configure(fb.getPersistenceUnitInfo(), fb.getJpaPropertyMap());
-        // export the database schema
-        SchemaExport schemaExport = new SchemaExport(configured.getHibernateConfiguration());
-
-        schemaExport.setOutputFile("C:\\Users\\paola\\Desktop\\testing_schema.txt");
-        schemaExport.setFormat(true);
-        schemaExport.setDelimiter(";");
-        schemaExport.execute(true, false, false, true);
+//        LocalContainerEntityManagerFactoryBean fb = (LocalContainerEntityManagerFactoryBean) context.getBean("&entityManagerFactory");
+//
+//        Ejb3Configuration cfg = new Ejb3Configuration();
+//        Ejb3Configuration configured = cfg.configure(fb.getPersistenceUnitInfo(), fb.getJpaPropertyMap());
+//        // export the database schema
+//        SchemaExport schemaExport = new SchemaExport(configured.getHibernateConfiguration());
+//
+//        schemaExport.setOutputFile("C:\\Users\\paola\\Desktop\\testing_schema.txt");
+//        schemaExport.setFormat(true);
+//        schemaExport.setDelimiter(";");
+//        schemaExport.execute(true, false, false, true);
+        
+        context.getBeansOfType(MultipleComparisonsCorrector.class);
+        
+        System.out.println("test");
     }
 }
