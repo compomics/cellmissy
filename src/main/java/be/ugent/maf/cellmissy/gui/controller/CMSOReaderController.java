c716133b862cd77261428eff2076e3fa99440f2f
Gwendolien Sergeant <gwendolien.sergeant@ugent.be>
2017-06-27 16:19:35 +0200

    creation of cmso reader gui panel and controller

---
+++ src/main/java/be/ugent/maf/cellmissy/gui/controller/CMSOReaderController.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package be.ugent.maf.cellmissy.gui.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ *
+ * @author CompOmics Gwen
+ */
+public class CMSOReaderController {
+    
+    
+    // parent controller
+    @Autowired
+    private CellMissyController cellMissyController;
+}
