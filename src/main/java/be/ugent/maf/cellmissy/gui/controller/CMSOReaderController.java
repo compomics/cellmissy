29ba3cc640058eaba06a17b2cad4256388cf88ef
Gwendolien Sergeant <gwendolien.sergeant@ugent.be>
2017-07-25 14:19:22 +0200

    move isa parser to separate method. Finished Investigation file part

--- src/main/java/be/ugent/maf/cellmissy/gui/controller/CMSOReaderController.java
+++ src/main/java/be/ugent/maf/cellmissy/gui/controller/CMSOReaderController.java
@@ -11,14 +11,22 @@ import java.awt.GridBagConstraints;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
 import loci.formats.FormatException;
 import loci.formats.IFormatReader;
 import loci.formats.in.OMEXMLReader;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.apache.commons.io.FileUtils;
@@ -102,12 +110,6 @@ public class CMSOReaderController {
         cmsoReaderPanel.getBiotracksTextArea().setText("");
     }
 
-    // need to read isa, ome and ../dp stuff and display
-    //implement parsers (interface-impl) ??
-    // --> what to return? String / Map<String, String> ("isa":"...info")
-    // (to return a new class seems dumb)
-    // ++ what in case of multiple  datapackages? show all in text
-    //      and force user to choose one for analysis when continuing
     /**
      *
      * @param file A folder that contains a CMSO dataset
@@ -119,7 +121,6 @@ public class CMSOReaderController {
 
             File[] entireDataset = datasetFolder.listFiles();
             File[] isaFiles;
-            File omeFile;
             List<File> biotracksFolders = new ArrayList<>(); //separate folder per tracking software used
 
             for (File file : entireDataset) {
@@ -130,7 +131,6 @@ public class CMSOReaderController {
                     String summaryText = "";
                     //read file
                     summaryText += FileUtils.readFileToString(file, "UTF-8");
-
                     cmsoReaderPanel.getSummaryTextArea().setText(summaryText);
                 }
 
@@ -138,24 +138,15 @@ public class CMSOReaderController {
                 if (name.endsWith("isa")) {
                     isaFiles = file.listFiles();
 
-                    String isaText = "";
-                    for (File isaFile : isaFiles) {
-                        isaText += isaFile.getName() + "\n";
-
-                        //check file name and get appropriate summary information
-                        // Study and assay files: rows = replicate, columns = data
-                        // since we want general information, only parse investigation file
-                    }
-
-                    cmsoReaderPanel.getIsaTextArea().setText(isaText);
+                    String isaSummary = parseISAFiles(isaFiles);
+                    cmsoReaderPanel.getIsaTextArea().setText(isaSummary);
 
                 } //Search for ome companion file. Array will contain only one file
                 else if (name.endsWith("companion.ome")) {
-                    omeFile = file;
                     IFormatReader reader = new OMEXMLReader();
-                    reader.setId(omeFile.getAbsolutePath());
+                    reader.setId(file.getAbsolutePath());
 
-                    String omeText = "File name: " + omeFile.getName();
+                    String omeText = "File name: " + file.getName() + "\n";
                     omeText += "Total amount of images = " + reader.getImageCount() + "\n";
                     omeText += "Dataset structure: " + reader.getDatasetStructureDescription() + "\n";
 
@@ -168,23 +159,23 @@ public class CMSOReaderController {
                     biotracksFolders.add(file);
 
                 }
+            }
+            String biotracksText = "";
+            if (!biotracksFolders.isEmpty()) {
+                // dp folders contain objects and links csv and json metadata
 
-                if (!biotracksFolders.isEmpty()) {
-                    // dp folders contain objects and links csv and json metadata
-                    String biotracksText = "";
-
-                    for (File trackingData : biotracksFolders) {
-                        // per folder, show: software name, total #objects, 
-                        // total #links + parameters contained in objects table (x,y,z,t...)
-                        biotracksText += "Software: " + trackingData.getName() + "\n";
-                        //only need the csv's
-                        File[] dp = trackingData.listFiles();
-                    }
-
-                    cmsoReaderPanel.getBiotracksTextArea().setText(biotracksText);
+                for (File trackingData : biotracksFolders) {
+                    // per folder, show: software name, total #objects, 
+                    // total #links + parameters contained in objects table (x,y,z,t...)
+                    biotracksText += "Software: " + trackingData.getName() + "\n";
+                    //only need the csv's
+                    File[] dp = trackingData.listFiles();
                 }
 
+            } else {
+                biotracksText += "No tracks files present in the dataset.";
             }
+            cmsoReaderPanel.getBiotracksTextArea().setText(biotracksText);
 
         } catch (FormatException ex) {
             LOG.error(ex.getMessage());
@@ -194,4 +185,109 @@ public class CMSOReaderController {
             cellMissyController.showMessage(ex.getMessage(), "Something seems wrong with the input", JOptionPane.ERROR_MESSAGE);
         }
     }
-}
+
+    private String parseISAFiles(File[] isaFiles) {
+        // initialize return
+        String isaText = "";
+        String[] isaTextArray = new String[3];
+
+        // parser and reader
+        CSVParser csvFileParser;
+        FileReader fileReader;
+        CSVFormat csvFileFormat;
+        // fileformat specification depending on delimination
+        csvFileFormat = CSVFormat.TDF;
+
+        for (File isaFile : isaFiles) {
+            // check name for correct spot in text array
+            if (isaFile.getName().startsWith("i")) {
+                String text = "";
+
+                // initialize the file reader and parser
+                try {
+                    fileReader = new FileReader(isaFile);
+                    csvFileParser = new CSVParser(fileReader, csvFileFormat);
+                    // get the csv records (rows)
+                    List<CSVRecord> csvRecords = csvFileParser.getRecords();
+
+                    // define which terms to show in the summary, set is fastest
+                    List<String> investigationTerms = new ArrayList<String>(
+                            Arrays.asList("Investigation Identifier", "Investigation Title", "Investigation Description",
+                                    "Investigation Submission Date", "Investigation Publication DOI", "Investigation Publication Author List", "Investigation Publication Title",
+                                    "Study Identifier", "Study Title", "Study Description", "Study File Name", "Study Publication DOI",
+                                    "Study Publication Author List", "Study Design Type", "Study Factor Name", "Study Factor Type",
+                                    "Study Assay Measurement Type", "Study Assay Technology Type", "Study Assay Technology Platform",
+                                    "Study Assay File Name", "Study Protocol Name", "Study Protocol Type", "Study Protocol Description",
+                                    "Study Protocol Parameters Name", "Study Protocol Components Name", "Study Protocol Components Type"));
+                    Set<String> invSummaryTerms = new HashSet<String>(investigationTerms);
+
+                    // read the CSV file records
+                    for (int row = 0; row < csvRecords.size(); row++) {
+                        CSVRecord cSVRecord = csvRecords.get(row);
+
+                        if (invSummaryTerms.contains(cSVRecord.get(0))) {
+                            //create an iterator for the record (row) values
+                            Iterator<String> iter = cSVRecord.iterator();
+                            //add all text parts to variable
+                            text += iter.next() + ": ";
+                            while (iter.hasNext()) {
+                                text += iter.next() + " // ";
+                            }
+                            text += "\n";
+
+                        }
+
+                    }
+                } catch (IOException ex) {
+                    LOG.error(ex.getMessage() + "/n Error while parsing Investigation file", ex);
+                }
+
+                isaTextArray[0] = text;
+
+            } else if (isaFile.getName().startsWith("s")) {
+                String text = "";
+                // initialize the file reader and parser
+                try {
+                    fileReader = new FileReader(isaFile);
+                    csvFileParser = new CSVParser(fileReader, csvFileFormat);
+                    // get the csv records (rows)
+                    List<CSVRecord> csvRecords = csvFileParser.getRecords();
+
+                } catch (IOException ex) {
+                    LOG.error(ex.getMessage() + "/n Error while parsing Study file", ex);
+                }
+
+                isaTextArray[1] = text;
+
+            } else if (isaFile.getName().startsWith("a")) {
+                String text = "";
+                // initialize the file reader and parser
+                try {
+                    fileReader = new FileReader(isaFile);
+                    csvFileParser = new CSVParser(fileReader, csvFileFormat);
+                    // get the csv records (rows)
+                    List<CSVRecord> csvRecords = csvFileParser.getRecords();
+
+                } catch (IOException ex) {
+                    LOG.error(ex.getMessage() + "/n Error while parsing Assay file", ex);
+                }
+
+                isaTextArray[2] = text;
+            }
+            isaText += isaFile.getName() + "\n";
+
+            //check file name and get appropriate summary information
+            // Study and assay files: rows = replicate, columns = data
+            // since we want general information, only parse investigation file (...?)
+            List<String> miacmeAssay = 
+        
+        
+        
+        ["SampleName", "AssayName", "Assaytype", "imagingModality", "ImagingSequenceType", 
+                        "ObservationPeriod", "ChannelDefinition", "PixelSize", "RawDataFile"];
+                    
+    }
+        
+        isaText = isaTextArray.toString();
+        return isaText;
+    }
