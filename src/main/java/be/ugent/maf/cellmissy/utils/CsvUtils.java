9aa56820360d5f5026da19307005f09eeb322d9f
Gwendolien Sergeant <gwendolien.sergeant@ugent.be>
2018-10-05 16:38:59 +0200

    Added CsvUtils class for writing txt files with custom separator possible

---
+++ src/main/java/be/ugent/maf/cellmissy/utils/CsvUtils.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package be.ugent.maf.cellmissy.utils;
+
+import java.util.List;
+import java.io.IOException;
+import java.io.Writer;
+
+
+
+/**
+ * Utility class for csv files -- currently for writing
+ * 
+ * @author Gwendolien Sergeant
+ */
+public class CsvUtils {
+    
+    private static final char DEFAULT_SEPARATOR = ',';
+    
+    public static void writeLine(Writer w, List<String> values) throws IOException {
+        writeLine(w, values, DEFAULT_SEPARATOR, ' ');
+    }
+
+    public static void writeLine(Writer w, List<String> values, char separators) throws IOException {
+        writeLine(w, values, separators, ' ');
+    }
+
+    // csv format here: https://tools.ietf.org/html/rfc4180
+    private static String followCVSformat(String value) {
+
+        String result = value;
+        if (result.contains("\"")) {
+            result = result.replace("\"", "\"\"");
+        }
+        return result;
+
+    }
+
+    public static void writeLine(Writer w, List<String> values, char separators, char customQuote) throws IOException {
+
+        boolean first = true;
+
+        //default customQuote is empty
+
+        if (separators == ' ') {
+            separators = DEFAULT_SEPARATOR;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (String value : values) {
+            if (!first) {
+                sb.append(separators);
+            }
+            if (customQuote == ' ') {
+                sb.append(followCVSformat(value));
+            } else {
+                sb.append(customQuote).append(followCVSformat(value)).append(customQuote);
+            }
+
+            first = false;
+        }
+        sb.append("\n");
+        w.append(sb.toString());
+
+
+    }
+}
